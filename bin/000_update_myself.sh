0acb09b6d30563343eedafce8fca61f26b4fd4ba
Robert Nowotny <bitranox@users.noreply.github.com>
2019-06-28 15:58:20 +0200

    initial commit

--- bin/000_update_myself.sh
+++ bin/000_update_myself.sh
@@ -20,6 +20,7 @@ function update_myself {
     retry sudo git fetch --all
     sudo git reset --hard origin/master
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
+    echo "MYDIR = ${my_dir}"
     sudo chmod +x "${my_dir}"/lib_bash/*.sh
     sudo chmod +x "${my_dir}"/lib_install/*.sh
     wait_for_enter "Update der eigenen Skripte beendet"
