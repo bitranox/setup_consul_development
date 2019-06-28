eb28adecc6d050b4845ef1d472f61d752033b9f7
Robert Nowotny <bitranox@users.noreply.github.com>
2019-06-28 15:42:30 +0200

    initial commit

--- bin/000_update_myself.sh
+++ bin/000_update_myself.sh
@@ -17,7 +17,7 @@ function update_myself {
     wait_for_enter "Update der Skripte"
     install_essentials
     linux_update
-    retry sudo git pull
+    git pull https://github.com/bitranox/consul-dev-env-public.git
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
     sudo chmod +x "${my_dir}"/lib_bash/*.sh
     sudo chmod +x "${my_dir}"/lib_install/*.sh
