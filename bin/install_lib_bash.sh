f6a407ad95680f6aef4b129f4536dd5ae6d0cde1
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-06 18:15:38 +0200

    initial commit

--- bin/install_lib_bash.sh
+++ bin/install_lib_bash.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-include_dependencies  # we need to do that via a function to have local scope of my_dir
-
 current_dir="`dirname \"$0\"`"
 echo "current_dir: ${current_dir}"
 my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
 echo "my_dir: ${current_dir}"
+
