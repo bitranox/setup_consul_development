220a5305bcff68a98d8a80927f0fd2fdc6973990
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-07 17:59:02 +0200

    initial commit

--- bin/install_lib_bash.sh
+++ bin/install_lib_bash.sh
@@ -32,10 +32,10 @@ function get_sudo_command_prefix {
 function set_lib_bash_permissions {
     local sudo_command_prefix=$(get_sudo_command_prefix)
     echo "sudo command prefix : ${sudo_command_prefix}"
-    ${sudo_command_prefix} chmod -R 0644 "/usr/lib/lib_bash"
-    ${sudo_command_prefix} chmod -R +x "/usr/lib/lib_bash/*.sh"
-    ${sudo_command_prefix} chown -R root "/usr/lib/lib_bash"
-    ${sudo_command_prefix} chgrp -R root "/usr/lib/lib_bash"
+    ${sudo_command_prefix} chmod -R 0644 /usr/lib/lib_bash
+    ${sudo_command_prefix} chmod -R +x /usr/lib/lib_bash/*.sh
+    ${sudo_command_prefix} chown -R root /usr/lib/lib_bash
+    ${sudo_command_prefix} chgrp -R root /usr/lib/lib_bash
 }
 
 function install_lib_bash_if_not_exist {
