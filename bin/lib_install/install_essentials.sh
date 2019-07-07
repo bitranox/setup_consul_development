e35ebcc0796d862d0dca8165c7441cee83ca5c54
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-07 18:28:28 +0200

    initial commit

--- bin/lib_install/install_essentials.sh
+++ bin/lib_install/install_essentials.sh
@@ -3,9 +3,9 @@
 function include_dependencies {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
     sudo chmod +x "${my_dir}"/../lib_bash/*.sh
-    source "${my_dir}/../lib_bash/lib_color.sh"
-    source "${my_dir}/../lib_bash/lib_retry.sh"
-    source "${my_dir}/../lib_bash/lib_helpers.sh"
+    source /usr/lib/lib_bash/lib_color.sh
+    source /usr/lib/lib_bash/lib_retry.sh
+    source /usr/lib/lib_bash/lib_helpers.sh
 }
 
 include_dependencies  # we need to do that via a function to have local scope of my_dir
