7ff6da86fad5e56ec4c3407fc77b160f76abdd2a
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-09 18:26:48 +0200

    initial commit

--- bin/menue-host.sh
+++ bin/menue-host.sh
@@ -55,13 +55,13 @@ function display_result {
 }
 
 function get_username {
-    exec 4>&1
+    exec 3>&1
     local result=$(dialog --title "Inputbox - To take input from you" \
         --backtitle "Linux Shell Script Tutorial Example" \
         --inputbox "Enter your name " ${INPUTBOX_HEIGHT} ${INPUTBOX_WIDTH} \
-        2>&1 1>&4)
+        2>&1 1>&3)
     local exit_status=$?
-    exec 4>&-
+    exec 3>&-
     echo ${result}
 }
 
