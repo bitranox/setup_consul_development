92af7d6bbd0911af870582d509aab9d51fb8d128
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-09 15:37:32 +0200

    initial commit

--- bin/lib_install.sh
+++ bin/lib_install.sh
@@ -7,28 +7,87 @@ function include_dependencies {
     source /usr/lib/lib_bash/lib_helpers.sh
 }
 
+
+function get_is_package_installed {
+    # $1: package name
+    local package_name=$1
+    if [[ $(($(dpkg -l ${package_name} | grep -c ${package_name}))) > 0  ]]; then
+        echo "True"
+    else
+        echo "False"
+    fi
+}
+
+
+function install_dialog {
+    if [[ "$(get_is_package_installed dialog)" == "False" ]]; then
+        retry $(get_sudo_command) apt-get install dialog -y
+    fi
+}
+
+function install_git {
+    if [[ "$(get_is_package_installed git)" == "False" ]]; then
+        retry $(get_sudo_command) apt-get install git -y
+    fi
+}
+
+function install_net_tools {
+    if [[ "$(get_is_package_installed net-tools)" == "False" ]]; then
+        retry $(get_sudo_command) apt-get install net-tools -y
+    fi
+}
+
+function uninstall_whoopsie {
+    local sudo_command=$(get_sudo_command)
+    if [[ "$(get_is_package_installed whoopsie)" == "True" ]]; then
+        retry ${sudo_command} apt-get purge whoopsie -y
+    fi
+    if [[ "$(get_is_package_installed libwhoopsie0)" == "True" ]]; then
+        retry ${sudo_command} apt-get purge libwhoopsie0 -y
+    fi
+    if [[ "$(get_is_package_installed libwhoopsie-preferences0)" == "True" ]]; then
+        retry ${sudo_command} apt-get purge libwhoopsie-preferences0 -y
+    fi
+}
+
+function uninstall_apport {
+    if [[ "$(get_is_package_installed apport)" == "True" ]]; then
+        retry $(get_sudo_command) apt-get purge apport -y
+    fi
+}
+
+
 function install_essentials {
     # update / upgrade linux and clean / autoremove
-    clr_bold clr_green " "
     clr_bold clr_green "Installiere Essentielles am Host, entferne Apport und Whoopsie"
-    local sudo_command=$(get_sudo_command)
-    retry ${sudo_command} apt-get install net-tools -y
-    retry ${sudo_command} apt-get install git -y
-    ${sudo_command} apt-get purge whoopsie -y
-    ${sudo_command} apt-get purge libwhoopsie0 -y
-    ${sudo_command} apt-get purge libwhoopsie-preferences0 -y
-    ${sudo_command} apt-get purge apport -y
+    install_net_tools
+    install_git
+    install_dialog
+    uninstall_whoopsie
+    uninstall_apport
 }
 
 function install_and_update_language_packs {
     # install language pack and install language files for applications
     banner "Install and Update Language Packs"
     local sudo_command=$(get_sudo_command)
-    retry ${sudo_command} apt-get install language-pack-de -y
-    retry ${sudo_command} apt-get install language-pack-de-base -y
-    retry ${sudo_command} apt-get install manpages-de -y
-    retry ${sudo_command} apt-get install language-pack-gnome-de -y
+
+    if [[ "$(get_is_package_installed language-pack-de)" == "True" ]]; then
+        retry ${sudo_command} apt-get install language-pack-de -y
+    fi
+    if [[ "$(get_is_package_installed language-pack-de-base)" == "True" ]]; then
+        retry ${sudo_command} apt-get install language-pack-de-base -y
+    fi
+    if [[ "$(get_is_package_installed manpages-de)" == "True" ]]; then
+        retry ${sudo_command} apt-get install manpages-de -y
+    fi
+    if [[ "$(get_is_package_installed language-pack-gnome-de)" == "True" ]]; then
+        retry ${sudo_command} apt-get install language-pack-gnome-de -y
+    fi
+
     ${sudo_command} update-locale LANG=\"de_AT.UTF-8\" LANGUAGE=\"de_AT:de\"
+
+    # TODO - put in an array and check for each one if it is installed already ...
     retry ${sudo_command} apt-get install $(check-language-support -l de) -y
 }
 
