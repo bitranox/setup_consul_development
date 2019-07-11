dec0ec94af2f611430d43099d804a2c45042fe07
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-11 23:40:22 +0200

    initial commit

--- 005_000_install_lxd_container_system.sh
+++ 005_000_install_lxd_container_system.sh
@@ -3,6 +3,7 @@
 function update_myself {
     /usr/local/setup_consul_development/install_or_update.sh "${@}" || exit 0              # exit old instance after updates
 }
+update_myself ${0} ${@}  # pass own script name and parameters
 
 
 function include_dependencies {
@@ -11,30 +12,12 @@ function include_dependencies {
     source /usr/local/lib_bash/lib_helpers.sh
     source /usr/local/lib_bash_install/900_000_lib_install_basics.sh
 }
-
-function install_lxd_container_system {
-    banner "snap Install LXD"
-    # install snap
-    retry $(which sudo) apt-get install snap -y
-    # install lxd
-    retry $(which sudo) snap install lxd
-}
-
-
-function add_user_to_lxd_group {
-    banner "LXD Init"
-    # add current user to lxd group
-    $(which sudo) usermod --append --groups lxd "${USER}"
-    # join the group for this session - not as root !
-    # init LXD - not as root !
-}
-
-update_myself ${0} ${@}  # pass own script name and parameters
 include_dependencies
+
 wait_for_enter "Installiere LXD Container System"
 install_essentials
 linux_update
 install_lxd_container_system
-add_user_to_lxd_group
+add_user_to_lxd_group "${USER}"
 wait_for_enter_warning "LXD Container System fertig installiert${IFS}ein Neustart ist erforderlich, Enter rebootet die Maschine${IFS}offene Dokumente vorher sichern !"
 reboot
