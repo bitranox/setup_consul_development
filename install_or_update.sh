f6d81213113f3af41ca84151d93855d87cd7ce32
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-14 20:15:25 +0200

    refractor

--- install_or_update.sh
+++ install_or_update.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+export bitranox_debug="True"
+
+
 function install_or_update_lib_bash {
     if [[ -f "/usr/local/lib_bash/install_or_update.sh" ]]; then
-        # $(which sudo) /usr/local/lib_bash/install_or_update.sh
-        echo "lib_bash already installed"
+        source /usr/local/lib_bash/lib_color.sh
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@install_or_update_lib_bash: lib_bash already installed, calling /usr/local/lib_bash/install_or_update.sh"; fi
+        $(which sudo) /usr/local/lib_bash/install_or_update.sh
     else
+        if [[ "${bitranox_debug}" == "True" ]]; then echo "setup_consul_development\install_or_update.sh@install_or_update_lib_bash: installing lib_bash"; fi
         $(which sudo) rm -fR /usr/local/lib_bash
         $(which sudo) git clone https://github.com/bitranox/lib_bash.git /usr/local/lib_bash > /dev/null 2>&1
         $(which sudo) chmod -R 0755 /usr/local/lib_bash
@@ -18,9 +23,10 @@ install_or_update_lib_bash
 
 function install_or_update_lib_bash_install {
     if [[ -f "/usr/local/lib_bash_install/install_or_update.sh" ]]; then
-        # $(which sudo) /usr/local/lib_bash_install/install_or_update.sh
-        echo "lib_bash_install already installed"
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@install_or_update_lib_bash_install: lib_bash_install already installed, calling /usr/local/lib_bash_install/install_or_update.sh"; fi
+        $(which sudo) /usr/local/lib_bash_install/install_or_update.sh
     else
+        if [[ "${bitranox_debug}" == "True" ]]; then echo "setup_consul_development\install_or_update.sh@install_or_update_lib_bash_install: installing lib_bash_install"; fi
         $(which sudo) rm -fR /usr/local/lib_bash_install
         $(which sudo) git clone https://github.com/bitranox/lib_bash_install.git /usr/local/lib_bash_install > /dev/null 2>&1
         $(which sudo) chmod -R 0755 /usr/local/lib_bash_install
@@ -59,13 +65,13 @@ function is_setup_consul_development_installed {
 }
 
 
-function is_setup_consul_development_to_update {
+function is_setup_consul_development_up_to_date {
     local git_remote_hash=$(git --no-pager ls-remote --quiet https://github.com/bitranox/setup_consul_development.git | grep HEAD | awk '{print $1;}' )
     local git_local_hash=$( $(which sudo) cat /usr/local/setup_consul_development/.git/refs/heads/master)
     if [[ "${git_remote_hash}" == "${git_local_hash}" ]]; then
-        echo "False"
-    else
         echo "True"
+    else
+        echo "False"
     fi
 }
 
@@ -80,39 +86,41 @@ function install_setup_consul_development {
 function restart_calling_script {
     local caller_command=("$@")
     if [ ${#caller_command[@]} -eq 0 ]; then
-        echo "setup_consul_development/install_or_update.sh : no caller command - exit 0"
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@restart_calling_script: no caller command - exit 0"; fi
         # no parameters passed
         exit 0
     else
         # parameters passed, running the new Version of the calling script
-        echo "setup_consul_development/install_or_update.sh : calling command : $@ - exit 100"
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@restart_calling_script: calling command : ${@}"; fi
         "${caller_command[@]}"
-        # exit this old instance with error code 100
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@restart_calling_script: after calling command : ${@} - exiting with 100"; fi
         exit 100
     fi
 }
 
 
 function update_setup_consul_development {
-    if [[ $(is_setup_consul_development_to_update) == "True" ]]; then
-        clr_green "setup_consul_development needs to update"
-        (
-            # create a subshell to preserve current directory
-            cd /usr/local/setup_consul_development
-            $(which sudo) git fetch --all  > /dev/null 2>&1
-            $(which sudo) git reset --hard origin/master  > /dev/null 2>&1
-            set_setup_consul_development_permissions
-        )
-        clr_green "setup_consul_development update complete"
-    else
-        clr_green "setup_consul_development is up to date"
-    fi
+    if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@update_setup_consul_development: updating setup_consul_development"; fi
+    (
+        # create a subshell to preserve current directory
+        cd /usr/local/setup_consul_development
+        $(which sudo) git fetch --all  > /dev/null 2>&1
+        $(which sudo) git reset --hard origin/master  > /dev/null 2>&1
+        set_setup_consul_development_permissions
+    )
+    if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@update_setup_consul_development: setup_consul_development update complete"; fi
 }
 
-
-if [[ $(is_setup_consul_development_installed) == "True" ]]; then
-    update_setup_consul_development
-    restart_calling_script  "${@}" || exit 0  # needs caller name and parameters
+if [[ $(is_lib_bash_install_installed) == "True" ]]; then
+    if [[ $(is_lib_bash_install_up_to_date) == "False" ]]; then
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@main: setup_consul_development is not up to date"; fi
+        update_setup_consul_development
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@main: call restart_calling_script ${@}"; fi
+        restart_calling_script  "${@}"
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@main: call restart_calling_script ${@} returned ${?}"; fi
+    else
+        if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@main: setup_consul_development is up to date"; fi
+    fi
 else
     install_setup_consul_development
 fi
