66c3bc3b3e0d43559173aea4e5f7f92c39d660f2
Robert Nowotny <bitranox@users.noreply.github.com>
2019-07-14 20:59:00 +0200

    refractor

--- install_or_update.sh
+++ install_or_update.sh
@@ -112,7 +112,7 @@ function update_setup_consul_development {
 }
 
 if [[ $(is_setup_consul_development_installed) == "True" ]]; then
-    if [[ $(is_lib_bash_install_up_to_date) == "False" ]]; then
+    if [[ $(is_setup_consul_development_up_to_date) == "False" ]]; then
         if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@main: setup_consul_development is not up to date"; fi
         update_setup_consul_development
         if [[ "${bitranox_debug}" == "True" ]]; then clr_blue "setup_consul_development\install_or_update.sh@main: call restart_calling_script ${@}"; fi
