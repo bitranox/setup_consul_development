4956a4e600ad8f63ee5eb9ee3bcd944eaf229fff
bitranox <rnowotny1966@gmail.com>
2019-07-17 14:07:47 +0200

    refractor

--- old/snippets.sh
+++ old/snippets.sh
@@ -2,5 +2,15 @@
 
 function include_dependencies {
     local my_dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"  # this gives the full path, even for sourced scripts
+
+
+}
+
+# works for sourced scripts also
+function get_my_dir {
+    local mydir=""
+    mydir="${BASH_SOURCE%/*}"
+    if [[ ! -d "$mydir" ]]; then mydir="$PWD"; fi
+    echo "$mydir"
 }
 
