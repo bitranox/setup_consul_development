38d27a671505ad00f46bc40dd6bcc7d91f2e0a68
bitranox <rnowotny1966@gmail.com>
2020-06-19 20:10:07 +0200

    refractor

--- build_docs.py
+++ build_docs.py
@@ -69,23 +69,23 @@ def main(args):
     logger.info('replace repository related strings')
     rst_str_replace(source='./.docs/README_template_included.rst',
                     target='./.docs/README_template_repo_replaced.rst',
-                    old='{repository_slug}',
-                    new=travis_repo_slug)
+                    str_pattern='{repository_slug}',
+                    str_replace=travis_repo_slug)
     rst_str_replace(source='./.docs/README_template_repo_replaced.rst',
                     target='./.docs/README_template_repo_replaced2.rst',
-                    old='{repository}',
-                    new=repository)
+                    str_pattern='{repository}',
+                    str_replace=repository)
     rst_str_replace(source='./.docs/README_template_repo_replaced2.rst',
                     target='./.docs/README_template_repo_replaced3.rst',
-                    old='{repository_dashed}',
-                    new=repository_dashed)
+                    str_pattern='{repository_dashed}',
+                    str_replace=repository_dashed)
 
     rst_include.rst_str_replace(source='./README.rst', target='', old='{last_update_yyyy}', new=str(datetime.date.today().year+1), inplace=True)
 
     rst_str_replace(source='./.docs/README_template_repo_replaced3.rst',
                     target='./README.rst',
-                    old='{codeclimate_link_hash}',
-                    new=codeclimate_link_hash)
+                    str_pattern='{codeclimate_link_hash}',
+                    str_replace=codeclimate_link_hash)
 
     logger.info('cleanup')
     os.remove('./.docs/README_template_included.rst')
